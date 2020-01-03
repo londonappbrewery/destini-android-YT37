13e45bf7893dc3d906f3043843046ebb3b51c750
Yug Thapar <yugthapar37@gmail.com>
2020-01-03 18:00:23 +0530

    Completed Destini Project

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,8 +2,16 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mStory;
+    private Button mCh1;
+    private Button mCh2;
+    private int mIndex = 1;
 
     // TODO: Steps 4 & 8 - Declare member variables here:
 
@@ -13,18 +21,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mStory = (TextView) findViewById(R.id.storyTextView);
+        mCh1 = (Button) findViewById(R.id.buttonTop);
+        mCh2 = (Button) findViewById(R.id.buttonBottom);
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mCh1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mIndex == 0) {
+                    mCh2.setVisibility(View.VISIBLE);
+                    mStory.setText(R.string.T1_Story);
+                    mCh1.setText(R.string.T1_Ans1);
+                    mCh2.setText(R.string.T1_Ans2);
+                    mIndex = 1;
+                } else if (mIndex == 1) {
+                    mIndex = 3;
+                    mStory.setText(R.string.T3_Story);
+                    mCh1.setText(R.string.T3_Ans1);
+                    mCh2.setText(R.string.T3_Ans2);
+                } else if (mIndex == 3) {
+                    mCh2.setVisibility(View.GONE);
+                    mCh1.setText(R.string.restart);
+                    mStory.setText(R.string.T6_End);
+                    mIndex = 0;
 
+                } else if (mIndex == 2) {
+                    mIndex = 3;
+                    mStory.setText(R.string.T3_Story);
+                    mCh1.setText(R.string.T3_Ans1);
+                    mCh2.setText(R.string.T3_Ans2);
+                }
 
+            }
+        });
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        mCh2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mIndex == 1) {
+                    mIndex = 2;
+                    mStory.setText(R.string.T2_Story);
+                    mCh1.setText(R.string.T2_Ans1);
+                    mCh2.setText(R.string.T2_Ans2);
+                } else if (mIndex == 2) {
+                    mCh2.setVisibility(View.GONE);
+                    mCh1.setText(R.string.restart);
+                    mStory.setText(R.string.T4_End);
+                    mIndex = 0;
+                } else if (mIndex == 3) {
+                    mCh2.setVisibility(View.GONE);
+                    mCh1.setText(R.string.restart);
+                    mStory.setText(R.string.T5_End);
+                    mIndex = 0;
+                }
 
-
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
-
+            }
+        });
     }
 }
